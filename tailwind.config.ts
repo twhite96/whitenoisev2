528971b55f83293d58f6645c727c669777dc9887
bufferstack <github@0x8c.org>
2024-12-25 15:28:56 -0500

    Update something

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
   future: {
     hoverOnlyWhenSupported: true,
   },
